8b782f90f75eba206490565774423861d33a5b2a
Starlin Gil Cruz <starlin.gil.cruz@gmail.com>
2023-10-24 01:14:45 -0400

    Fix issue with consumer key

---
+++ broker/broker.go
@@ -0,0 +1,137 @@
+package broker
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/starlingilcruz/golang-broker/api"
+	"github.com/starlingilcruz/golang-broker/utils"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type StockRequest struct {
+	RoomName string `json:"roomName"`
+	RoomId   uint   `json:"roomId"`
+	Message  string `json:"message"`
+}
+
+type StockReponse struct {
+	RoomId  uint   `json:"RoomId"`
+	Message string `json:"Message"`
+}
+
+type Broker struct {
+	ReceiverQueue  amqp.Queue
+	PublisherQueue amqp.Queue
+	Channel        *amqp.Channel
+}
+
+func (b *Broker) SetUp(ch *amqp.Channel) {
+	receiverQueue := os.Getenv("BR_CONSUMER_QUEUE")
+	publisherQueue := os.Getenv("BR_PRODUCER_QUEUE")
+
+	q1, err := ch.QueueDeclare(
+		receiverQueue, // name
+		false,         // durable
+		false,         // delete when unused
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // arguments
+	)
+	utils.FailOnError(err, "Failed to declare"+receiverQueue+" queue")
+
+	q2, err := ch.QueueDeclare(
+		publisherQueue, // name
+		false,          // durable
+		false,          // delete when unused
+		false,          // exclusive
+		false,          // no-wait
+		nil,            // arguments
+	)
+	utils.FailOnError(err, "Failed to declare "+publisherQueue+" queue")
+
+	b.ReceiverQueue = q1
+	b.PublisherQueue = q2
+	b.Channel = ch
+}
+
+func (b *Broker) PublishMessage(sr StockReponse) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body, err := json.Marshal(sr)
+	if err != nil {
+		log.Printf("Response structure error %s ", err)
+	}
+
+	err = b.Channel.PublishWithContext(ctx,
+		"",                    // exchange
+		b.PublisherQueue.Name, // routing key
+		false,                 // mandatory
+		false,                 // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        body,
+		})
+	utils.FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
+
+func (b *Broker) ReadMessages() {
+	msgs, err := b.Channel.Consume(
+		b.ReceiverQueue.Name, // queue
+		"",                   // consumer
+		true,                 // auto-ack
+		false,                // exclusive
+		false,                // no-local
+		false,                // no-wait
+		nil,                  // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	rsvdMsgs := make(chan StockRequest)
+	go messageTransformer(msgs, rsvdMsgs)
+	go processRequest(rsvdMsgs, b)
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+}
+
+func messageTransformer(entries <-chan amqp.Delivery, receivedMessages chan StockRequest) {
+	var sr StockRequest
+	for d := range entries {
+		log.Println("d.Body", string(d.Body))
+		err := json.Unmarshal([]byte(d.Body), &sr)
+		if err != nil {
+			log.Printf("Error on received request : %s ", err)
+			continue
+		}
+		log.Println("Received a request")
+		receivedMessages <- sr
+	}
+}
+
+func processRequest(s <-chan StockRequest, b *Broker) {
+
+	for r := range s {
+		log.Println("processing stock request for ", r.RoomId)
+		cM := r.Message
+		cM = strings.Replace(cM, "/stock=", "", 1)
+		sr := StockReponse{
+			RoomId:  r.RoomId,
+			Message: fmt.Sprintf("Processing: %s", cM),
+		}
+		go b.PublishMessage(sr)
+		msg := api.EvalStock(cM)
+		sr2 := StockReponse{
+			RoomId:  r.RoomId,
+			Message: msg,
+		}
+		go b.PublishMessage(sr2)
+		log.Println("processed", r.Message)
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/starlingilcruz/golang-broker/broker"
+	"github.com/starlingilcruz/golang-broker/utils"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func main() {
+var br broker.Broker
 
+func main() {
+	log.Println("Stock bot service starting ...")
+
+	err := godotenv.Load()
+	utils.FailOnError(err, "Error loading .env file")
+
+	rmqHost := os.Getenv("RMQ_HOST")
+	rmqUserName := os.Getenv("RMQ_USERNAME")
+	rmqPassword := os.Getenv("RMQ_PASSWORD")
+	rmqPort := os.Getenv("RMQ_PORT")
+	dsn := "amqp://" + rmqUserName + ":" + rmqPassword + "@" + rmqHost + ":" + rmqPort + "/"
+
+	log.Println(dsn)
+
+	conn, err := amqp.Dial(dsn)
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	br.SetUp(ch)
+	go br.ReadMessages()
+	select {}
 }
